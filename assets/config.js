dd08201a5c8373a5279ddd16c86e06203a3d6af5
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-28 19:42:44 +0600

    .

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://minus-barrier-bottles-sacred.trycloudflare.com",
+  API_URL: "https://illustrated-brook-renewable-low.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
