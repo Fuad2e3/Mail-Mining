3ce80e081d20c7e216391aa98163591a9036a7cc
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-28 19:41:21 +0600

    Sync live backend tunnel URL in assets/config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://netscape-profits-footage-rentals.trycloudflare.com",
+  API_URL: "https://minus-barrier-bottles-sacred.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
